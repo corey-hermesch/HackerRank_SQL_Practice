74fe8968722a83b2779c06870b31b0ff491955c3
Corey Hermesch <corey.hermesch@gmail.com>
2023-08-03 14:59:01 -0500

    weather station problems complete

--- weather-station-problems.sql
+++ weather-station-problems.sql
@@ -1,4 +1,7 @@
-/*
+## HackerRank MySQL weather station Problems ##
+
+######## 1. 
+/* 
 Query the two cities in STATION with the shortest and longest CITY names, 
 as well as their respective lengths (i.e.: number of characters in the name). 
 If there is more than one smallest or largest city, choose the one that comes 
@@ -77,7 +80,8 @@ FROM (
     ) as u
 ;
 */
-######### NEXT Problem
+
+######## 2. 
 /* Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. 
 Your result cannot contain duplicates.
 */
@@ -121,3 +125,85 @@ WHERE
 ORDER BY CITY
 ;
 */
+
+####### 3.
+/* Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) 
+as both their first and last characters. Your result cannot contain duplicates.*/
+SELECT DISTINCT service_request_type
+FROM cases
+WHERE 
+	service_request_type REGEXP '^[aeiouAEIOU].*[aeiouAEIOU]$'
+;
+/* HERE IS THE SOLUTION TO THE HACKER RANK PROBLEM */
+/*
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY REGEXP '^[AEIOUaeiou].*[AEIOUaeiou]$'
+;
+*/
+
+######## 4. 
+/* Query the list of CITY names from STATION that do not start with vowels. 
+Your result cannot contain duplicates. */
+SELECT DISTINCT service_request_type
+FROM cases
+WHERE 
+	service_request_type REGEXP '^[^aeiouAEIOU].*'
+;
+/* HERE IS THE SOLUTION TO THE HACKER RANK PROBLEM */
+/*
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY REGEXP '^[^AEIOUaeiou]'
+;
+*/
+
+######## 5. 
+/* Query the list of CITY names from STATION that do not end with vowels. 
+Your result cannot contain duplicates. */
+
+SELECT DISTINCT service_request_type
+FROM cases
+WHERE 
+	service_request_type REGEXP '^[^aeiouAEIOU].*'
+;
+/* HERE IS THE SOLUTION TO THE HACKER RANK PROBLEM */
+/*
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY REGEXP '[^aeiouAEIOU]$'
+;
+*/
+
+####### 6. 
+/* Query the list of CITY names from STATION that either do not start with vowels 
+or do not end with vowels.  Your result cannot contain duplicates.
+*/
+SELECT DISTINCT service_request_type
+FROM cases
+WHERE 
+	service_request_type REGEXP '^[^aeiouAEIOU]'
+    OR service_request_type REGEXP '[^aeiouAEIOU]$'
+;
+
+/* HERE IS THE SOLUTION TO THE HACKER RANK PROBLEM
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY REGEXP '^[^aeiouAEIOU]'
+    OR CITY REGEXP '[^aeiouAEIOU]$'
+    ;
+*/
+
+######## 7.
+/* Query the list of CITY names from STATION that do not start with vowels 
+and do not end with vowels. Your result cannot contain duplicates. */
+/* HERE IS THE SOLUTION TO THE HACKER RANK PROBLEM 
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY REGEXP '^[^aeiouAEIOU].*[^aeiouAEIOU]$'
+;
+*/
+
+
+
+
